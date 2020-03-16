67cffc65502296cc825580814cbf272f71a56199
Oles <ruboken@gmail.com>
2020-03-16 16:09:06 +0200

    power slider finish

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,16 @@
 $(".two-slide-slider").slick({
     speed:1000,
     infinite: false,
-    centerMode:true,
+    // centerMode:true,
+});
 
-
+$('.power-slider').slick({
+    speed:1000,
+    slidesToShow: 1,
+    centerMode: true,
+    centerPadding: 0,
+    dots: true,
+    waitForAnimate: true,
+    infinite: false
 });
 
