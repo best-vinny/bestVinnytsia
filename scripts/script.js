6e95c6a9022d30753260ac3826f753a5b2730a88
Oles <ruboken@gmail.com>
2020-03-19 23:27:37 +0200

    first project finish

--- scripts/script.js
+++ scripts/script.js
@@ -17,21 +17,65 @@ $('.power-slider').slick({
 $('.projects-logo-slider').slick({
     slidesToShow: 3,
     slidesToScroll:1,
-    speed:1000,
-    autoplay: true,
+    speed:300,
+    // autoplay: true,
     centerMode: true,
-    focusOnSelect: true
+    focusOnSelect: true,
+    // pauseOnHover: true,
+    // pauseOnFocus: true,
+    draggable: false,
+    waitForAnimate: false,
+    asNavFor: '.current-projects-slider',
+});
+
+
+$('.current-projects-slider').slick({
+    slidesToShow: 1,
+    arrows: false,
+    focusOnSelect: true,
+    draggable: false,
+    swipe: false,
+    touchMove: false,
+    // autoplay: true,
+    // pauseOnHover: true,
+    // pauseOnFocus: true,
+    // asNavFor: '.projects-logo-slider',
+    // fade: true,
 });
 
 $('.project-main-photo').slick({
-    asNavFor: '.project-photo-nav',
-    autoplay: true,
+    arrows: false,
+    draggable: false,
+    swipe: false,
+    touchMove: false,
+    fade: true,
+    // asNavFor: '.project-photo-nav',
+    speed:1000,
 });
 
 
 $('.project-photo-nav').slick({
-    slidesToShow: 5,
-    slidesToScroll:1,
+    slidesToShow: 3,
+    slidesToScroll: 1,
     speed:1000,
-    asNavFor: '.project-main-photo',
+    // asNavFor: '.project-main-photo',
+    arrows: false,
+    vertical: true,
+    autoplay: true,
+    verticalSwiping: true,
+    pauseOnHover: true,
+    pauseOnFocus: true,
+    centerMode:true,
+    focusOnSelect: true,
+    draggable: false,
+    swipe: false,
+    touchMove: false,
+});
+
+const currentProjectsSlider = $('.current-projects-slider')
+
+$('.project-photo-nav').on('mousedown', function() {
+    currentProjectsSlider.slick.setOption({
+        swipe: false
+    })
 });
