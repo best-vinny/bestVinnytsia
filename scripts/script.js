eaede8fa751e5c7e6e6029f29b3232db00d380b6
Oles <ruboken@gmail.com>
2020-03-20 18:35:23 +0200

    project slider finish

--- scripts/script.js
+++ scripts/script.js
@@ -32,15 +32,10 @@ $('.projects-logo-slider').slick({
 $('.current-projects-slider').slick({
     slidesToShow: 1,
     arrows: false,
-    focusOnSelect: true,
     draggable: false,
     swipe: false,
     touchMove: false,
-    // autoplay: true,
-    // pauseOnHover: true,
-    // pauseOnFocus: true,
-    // asNavFor: '.projects-logo-slider',
-    // fade: true,
+    fade: true,
 });
 
 $('.project-main-photo').slick({
@@ -49,8 +44,7 @@ $('.project-main-photo').slick({
     swipe: false,
     touchMove: false,
     fade: true,
-    // asNavFor: '.project-photo-nav',
-    speed:1000,
+   autoplay: false
 });
 
 
@@ -58,24 +52,17 @@ $('.project-photo-nav').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     speed:1000,
-    // asNavFor: '.project-main-photo',
+    centerMode:true,
+    asNavFor: '.project-main-photo',
     arrows: false,
     vertical: true,
-    autoplay: true,
     verticalSwiping: true,
+    autoplay: true,
     pauseOnHover: true,
     pauseOnFocus: true,
-    centerMode:true,
     focusOnSelect: true,
     draggable: false,
     swipe: false,
     touchMove: false,
 });
 
-const currentProjectsSlider = $('.current-projects-slider')
-
-$('.project-photo-nav').on('mousedown', function() {
-    currentProjectsSlider.slick.setOption({
-        swipe: false
-    })
-});
