249d9ae1796402ef5219b1726c7960d066504839
Oles <ruboken@gmail.com>
2020-03-29 09:14:23 +0300

    mobile menu 100vh

--- scripts/script.js
+++ scripts/script.js
@@ -102,6 +102,22 @@ $(document).ready(function(){
         $(this).toggleClass('open');
         $('.menu').toggleClass('open-menu');
     });
+
+    $('.mobile-dd').click(function(){
+      $(this).toggleClass('open');
+    });
 });
 
+
+
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
 
